2870b629b7bdb3f1099d6466dfe3005634401637
arwi74 <seba.luk14@Gmail.com>
2018-09-01 19:46:48 +0200

    Add simple web engine

---
+++ app.js
@@ -0,0 +1,30 @@
+// === MODULES === //
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+const sync = require('sync-request');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(flash());
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.use('/', router);
+
+module.exports = app;
